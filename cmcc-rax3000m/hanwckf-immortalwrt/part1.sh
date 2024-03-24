468e9de1908cdd5ae1ec1b7eeaf99c4530e06344
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-24 21:08:26 +0800

    Update part1.sh

--- cmcc-rax3000m/hanwckf-immortalwrt/part1.sh
+++ cmcc-rax3000m/hanwckf-immortalwrt/part1.sh
@@ -14,14 +14,13 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 echo '添加Passwall依赖feeds'
-#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
-#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 #echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
-#echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 #echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 
 
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 echo '=========Add passwall feeds source OK!========='
 
