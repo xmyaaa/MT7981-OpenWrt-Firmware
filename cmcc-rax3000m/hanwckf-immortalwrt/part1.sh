9f886c19b9e531e713ab76834913c4a8596c9aea
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-27 01:19:35 +0800

    Update part1.sh

--- cmcc-rax3000m/hanwckf-immortalwrt/part1.sh
+++ cmcc-rax3000m/hanwckf-immortalwrt/part1.sh
@@ -22,8 +22,8 @@ echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.gi
 #echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 echo '=========Add passwall feeds source OK!========='
 
 # Add a feed source
