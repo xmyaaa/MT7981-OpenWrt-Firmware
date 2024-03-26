7a9fbf0c08e14d4602f207832e3747dffb7ae971
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-26 23:18:17 +0800

    Update part1.sh

--- cmcc-rax3000m/hanwckf-immortalwrt/part1.sh
+++ cmcc-rax3000m/hanwckf-immortalwrt/part1.sh
@@ -16,17 +16,17 @@
 echo '添加Passwall依赖feeds'
 #sed -i '1 i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages;main' feeds.conf.default
 #
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 #echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
-echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+#echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 #
 #svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-package/trunk/package/sing-box package/sing-box
+#svn co https://github.com/xiaorouji/openwrt-package/trunk/package/sing-box package/sing-box
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 echo '=========Add passwall feeds source OK!========='
 
 # Add a feed source
 rm -rf package/helloworld
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
