bac6068f7af2b65ace9ddc319bad0e8e75ba04df
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-31 07:55:35 +0800

    '修改Passwall检测规则'

--- cmcc-rax3000m/hanwckf-immortalwrt/part2.sh
+++ cmcc-rax3000m/hanwckf-immortalwrt/part2.sh
@@ -32,7 +32,7 @@ rm -rf feeds/luci/applications/luci-app-passwall/luci-app-passwall
 echo '=========Replace passwall source OK!========='
 
 echo '修改Passwall检测规则'
-sed -i 's/socket" "iptables-mod-//g' feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/app.sh
+#sed -i 's/socket" "iptables-mod-//g' feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/app.sh
 echo '=========ALTER passwall denpendcies check OK!========='
 
 # echo '开启sing-box的CGO标记'
