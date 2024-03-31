0f7b453e47eb461160c3c7183c403609143e6ff2
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-04-01 00:08:35 +0800

    Update part2.sh

--- cmcc-rax3000m/hanwckf-immortalwrt/part2.sh
+++ cmcc-rax3000m/hanwckf-immortalwrt/part2.sh
@@ -15,8 +15,8 @@
 #limitations under the License.
 
 echo '替换jerrykuku的luci argon主题'
-rm -rf feeds/luci/themes/luci-theme-argon
-git clone -b master --single-branch https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
+# rm -rf feeds/luci/themes/luci-theme-argon
+#git clone -b master --single-branch https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
 echo '=========Replace luci theme argon OK!========='
 
 echo '替换golang到1.22.x'
