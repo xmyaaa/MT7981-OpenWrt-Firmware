231c74f7420bbad42e62a272a085b2457fd1b2ef
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-04-01 08:35:44 +0800

    Update part1.sh

--- cmcc-rax3000m/hanwckf-immortalwrt/part1.sh
+++ cmcc-rax3000m/hanwckf-immortalwrt/part1.sh
@@ -15,8 +15,8 @@
 #limitations under the License.
 #
 echo '添加Passwall依赖feeds'
-sed -i '1 i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages;main' feeds.conf.default
+#sed -i '1 i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages;main' feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/Boos4721/OpenWrt-Packages' >>feeds.conf.default
 #echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 echo '=========Add passwall feeds source OK!========='
